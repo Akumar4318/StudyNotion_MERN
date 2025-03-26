389c562c333f58c86e834fe981bcc0fb3df7b63d
Akumar4318 <akumar4318@gmail.com>
2025-03-27 03:57:28 +0530

    Navbar created

--- Frontend/src/Components/common/Navbar.jsx
+++ Frontend/src/Components/common/Navbar.jsx
@@ -1,58 +1,145 @@
-import React from 'react'
-import { Link, matchPath,  } from 'react-router-dom'
-import Logo from "../../assets/Logo/Logo-Full-Light.png"
-import {NavbarLinks} from "../../data/navbar-links"
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, matchPath } from "react-router-dom";
+import Logo from "../../assets/Logo/Logo-Full-Light.png";
+import { NavbarLinks } from "../../data/navbar-links";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import ProfileDropDown from "../Auth/ProfileDropDown";
+import { categories } from "../../Services/api";
+import { apiConnector } from "../../Services/apiconnector";
+import { FaChevronDown } from "react-icons/fa6";
+
 
 const Navbar = () => {
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
+  
 
+    const [subLinks,setSubLinks]=useState('')
+    const [loading,setLoading]=useState(false)
 
-    const location=useLocation();
-    const matchRoute=(route)=>{
+    const fetchSublinks= async()=>{
+        setLoading(true)
+        try {
+            
+            const result= await apiConnector("GET",categories.CATEGORIES_API)
+            console.log(result.data.data)
+            setSubLinks(result.data.data)
 
-        return matchPath({path:route},location.pathname);
+         
+
+        } catch (error) {
+            console.log("Couldnot fetch the category list",error)
+          
+        }
+        setLoading(false)
+
     }
+    useEffect(()=>{
+
+        fetchSublinks()
+    },[])
+
+
+
+
+
+  const location = useLocation();
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
 
   return (
-    <div className='flex h-14 items-center justify-around border-b-[1px] border-b-richblack-700'>
-        <div className='flex w-11/12 max-w-maxContent  items-center '>
-
+    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 relative">
+      <div className="flex w-11/12 max-w-maxContent  items-center justify-between ">
         <Link to="/">
-        <img src={Logo} alt="" width={160} height={42} loading='lazy'/>
+          <img src={Logo} alt="" width={160} height={42} loading="lazy" />
         </Link>
-        
 
         {/* Navlinks */}
         <nav>
-            <ul className='flex gap-x-6 text-richblack-25 '>
-                {
-                    NavbarLinks.map((link,index)=>(
-                        <li
-                        key={index}>{
-                            link.title === "Catalog" ? (<di></di>):(
-                                <Link to={link?.path}>
-                                    <p className={`${matchRoute(link?.path)?"text-yellow-25":"text-richblack-25"}`}>{link.title}</p>
-                                </Link>
-                            ) 
-                        }
+          <ul className="flex gap-x-6 text-richblack-25 ">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <div className="flex items-center gap-2 cursor-pointer group relative">
+                        {link.title}
+                        <FaChevronDown/>
 
-                        </li>
+                        <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]
+                        ">
 
-                    ))
-                }
+                            <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5 ">
 
-            </ul>
+                            </div>
+                            {
+                               loading ? (
+                                <p className="text-center">Loading...</p>
+                               ):(
+                                subLinks.length ? (
+                                    subLinks.map((item,index)=>(
+                                        <Link className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50" key={index} to={`/catlog/${item.name}`} >
+                                        <p>{item.name}</p>
+                                        </Link>
+                                    ))
+                                   ):(<div></div>)
+                               )
+                            }
+
+                        </div>
+                  </div>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
         </nav>
 
         {/* login and signup Dashboard */}
 
-        <div className='flex gap-x-4 items-center'>
-            
+        <div className="flex gap-x-4 items-center">
+          {user && user?.accountType != "Instructor" && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="border border-richblack-700 bg-richblack-800  lg:px-[12px] lg:py-[8px] px-2 py-2 rounded-md text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="border border-richblack-700 bg-richblack-800  lg:px-[12px] lg:py-[8px] px-2 py-2 rounded-md text-richblack-100">
+                Sign Up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropDown />}
         </div>
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ Frontend/src/Services/api.js
@@ -0,0 +1,11 @@
+
+
+const BASE_URL = "http://localhost:3000/api/v1"
+
+
+
+
+export const categories={
+
+    CATEGORIES_API:BASE_URL+'/course/showallcategories'
+}
