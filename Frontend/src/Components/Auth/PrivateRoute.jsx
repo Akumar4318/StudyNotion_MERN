7448ad83e98bd456fc786ea5e8241848f90f32c6
Akumar4318 <akumar4318@gmail.com>
2025-04-13 03:53:03 +0530

    done

--- Frontend/src/Components/Auth/PrivateRoute.jsx
+++ Frontend/src/Components/Auth/PrivateRoute.jsx
@@ -5,8 +5,9 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({children}) => {
 
     const {token} = useSelector((state) => state.auth);
-
+    console.log("aman")
     if(token !== null)
+      
         return children
     else
         return <Navigate to="/login" />
